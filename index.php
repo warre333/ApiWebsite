c91be4eeafcd6bb247deeda0aa411596dc2189fc
warre333 <95884089+warre333@users.noreply.github.com>
2021-12-17 17:12:49 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<DOCTYPE html>
+<html>
+    <head>
+        <title>RoYoutuber</title>
+    </head>
+    <body>
+        <form>
+            <label for="YoutuberName">Name: </label>
+            <input type="text" name="YoutuberName">
+        </form>
+    </body>
+</html>
